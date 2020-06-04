843686e486c14ce5d7454670bc0213262dca10b5
gleison emanuel evangelista amorim <gleison.spfc1208@hotmail.com>
2020-06-04 09:19:12 -0300

    criação de arquivo na raiz de config do knex

---
+++ knexfile.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : '123456',
+      database : 'node'
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  }
+};
+
+ 
